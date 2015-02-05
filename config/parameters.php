b02390334c69badef1d8d63d84a7d6021b57c5f2
Thibaud BARDIN <thibaud.bardin@gmail.com>
2015-02-05 13:08:21 +0100

    Initial commit

---
+++ config/parameters.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => 'toor',
+    'dbname'   => 'pokedex',
+    'port'     => null,
+];
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'function/database.fn.php';
+
+$parameters = require 'config/parameters.php';
+
+database_connect($parameters);
